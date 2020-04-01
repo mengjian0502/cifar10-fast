e8c6526a53aa0f65a4a6a292e0efe549fc0a68a3
mengjian <jmeng15@asu.edu>
2020-03-31 22:58:01 -0700

    Remove TD in the input conv layer

--- model.py
+++ model.py
@@ -3,10 +3,17 @@ from torch_backend import *
 from td import Conv2d_TD, Linear_TD, Conv2d_col_TD
 
 # Network definition
-def conv_bn(c_in, c_out, gamma=0.0, alpha=0.0, block_size=16):
+def conv_bn_TD(c_in, c_out, gamma=0.0, alpha=0.0, block_size=16):
     return {
         # 'conv': nn.Conv2d(c_in, c_out, kernel_size=3, stride=1, padding=1, bias=False), 
         'conv': Conv2d_TD(c_in, c_out, kernel_size=3, stride=1, padding=1, bias=False, gamma=gamma, alpha=alpha, block_size=block_size), 
+        'bn': BatchNorm(c_out), 
+        'relu': nn.ReLU(True)
+    }
+
+def conv_bn(c_in, c_out):
+    return {
+        'conv': nn.Conv2d(c_in, c_out, kernel_size=3, stride=1, padding=1, bias=False), 
         'bn': BatchNorm(c_out), 
         'relu': nn.ReLU(True)
     }
@@ -24,9 +31,9 @@ def net(channels=None, weight=0.125, pool=nn.MaxPool2d(2), extra_layers=(), res_
     n = {
         'input': (None, []),
         'prep': conv_bn(3, channels['prep']),
-        'layer1': dict(conv_bn(channels['prep'], channels['layer1'], gamma=gamma, alpha=alpha, block_size=block_size), pool=pool),
-        'layer2': dict(conv_bn(channels['layer1'], channels['layer2'], gamma=gamma, alpha=alpha, block_size=block_size), pool=pool),
-        'layer3': dict(conv_bn(channels['layer2'], channels['layer3'], gamma=gamma, alpha=alpha, block_size=block_size), pool=pool),
+        'layer1': dict(conv_bn_TD(channels['prep'], channels['layer1'], gamma=gamma, alpha=alpha, block_size=block_size), pool=pool),
+        'layer2': dict(conv_bn_TD(channels['layer1'], channels['layer2'], gamma=gamma, alpha=alpha, block_size=block_size), pool=pool),
+        'layer3': dict(conv_bn_TD(channels['layer2'], channels['layer3'], gamma=gamma, alpha=alpha, block_size=block_size), pool=pool),
         'pool': nn.MaxPool2d(4),
         'flatten': Flatten(),
         'linear': nn.Linear(channels['layer3'], 10, bias=False),
